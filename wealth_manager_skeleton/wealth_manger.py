a7c7708cdc61e0ef6aa3bd9d9ae2db653e6af1f9
pawlaczyk <pawlaczyk@g.pl>
2020-11-04 01:29:43 +0100

    [TDD 4] Calculator class constructor

--- wealth_manager_skeleton/wealth_manger.py
+++ wealth_manager_skeleton/wealth_manger.py
@@ -1,5 +1,15 @@
 """
-@author: yourname
-@date: today's date
 This class is going to calculate how many years will it take to generate passive for a given income from renting apts.
 """
+
+
+class Calculator:
+    def __init__(self, passive_income_desired_yearly, yearly_savings, starting_year, price_of_one_apartment,
+                 price_od_renting_one_apartment):
+        self._passive_income_desired_yearly = passive_income_desired_yearly
+        self._yearly_savings = yearly_savings
+        self._starting_year = starting_year
+        self._price_of_one_apartment = price_of_one_apartment
+        self._price_od_renting_one_apartment = price_od_renting_one_apartment
+        self._anser = dict() #odpowiedx w postaci słownika
+        self._calculate()
